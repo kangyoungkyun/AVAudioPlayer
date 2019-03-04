28abf9939275f1455f4d3d75855553987c6eabf9
kang <abcnt@naver.com>
2019-03-04 14:11:58 +0900

    Update MusicBarView.m

--- AVAudioPlayer/MusicBarView.m
+++ AVAudioPlayer/MusicBarView.m
@@ -13,6 +13,7 @@
 @implementation MusicBarView
 
 
+//2. view 객체 7개 생성
 
 -(void) initlvar {
     _count = 7;
@@ -37,7 +38,7 @@
 }
 
 
-//초기화
+//1.초기화
 - (instancetype)init{
     self = [super init];
     if(nil != self){
@@ -46,18 +47,53 @@
     return self;
 }
 
+//3.초기화
+- (instancetype)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if (nil != self) {
+        [self initlvar];
+    }
+    return self;
+}
+
+//4.초기화
+- (instancetype)initWithCoder:(NSCoder *)aDecoder{
+    self = [super initWithCoder:aDecoder];
+    if (nil != self) {
+        [self initlvar];
+    }
+    return  self;
+}
 
 
+//추가
 -(void)addValue:(float)value{
-    
+    //_volumes이 Null 이 아니고 _volumes 개수가 7 보다 작으면
+    if (nil != _volumes && [_volumes count] < (_count)) {
+        [_volumes addObject:[NSNumber numberWithFloat:value]];
+    }else{
+        //_volumes이 null이고 _volumes 개수가 7 보다 크면
+        [_volumes addObject:[NSNumber numberWithFloat:value]];
+        [_volumes removeObjectAtIndex:0];
+    }
 }
 
+//삭제
 - (void) clear{
-    
+    for (UIView *v in _volumeViews) {
+        v.alpha = 0;
+    }
 }
 
-
+//업데이트
 -(void) updateUI{
+    
+    for (int i = 0; i < _count && i < [_volumes count]; i++) {
+        UIView *view = [_volumeViews objectAtIndex:i];
+        view.alpha = 1;
+        float volume = [((NSNumber *)[_volumes objectAtIndex:i]) floatValue] * self.frame.size.height;
+        [view setFrame:CGRectMake((2*i + 1 )* _unitWidth , self.frame.size.height - volume , _unitWidth, volume)];
+    }
     
 }
 
