bd9bde70bb82958a0e7377a8e9046a461a38d371
kang <abcnt@naver.com>
2019-03-04 13:18:51 +0900

    Initial Commit

---
+++ AVAudioPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVAudioPlayer
+//
+//  Created by 위피아 on 04/03/2019.
+//  Copyright © 2019 위피아. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVAudioPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVAudioPlayer
+//
+//  Created by 위피아 on 04/03/2019.
+//  Copyright © 2019 위피아. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
